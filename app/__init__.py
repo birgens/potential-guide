68bf473d9f9c83f1bf2bff8cafca3e170f9fe597
birgens <boirg@online.no>
2017-03-17 23:24:27 +0100

    oprdn

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template
 
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+from app.mod_auth.views import mod_auth as auth_module
+
+# Register blueprint(s)
+app.register_blueprint(auth_module)
+# app.register_blueprint(xyz_module)
+# ..
+
+db.create_all()
